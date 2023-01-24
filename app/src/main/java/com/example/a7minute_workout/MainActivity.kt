54e74455297dd1853a9d72c1198d55a4f2f54b8a
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-23 18:04:04 -0800

    Created Intent in the main activity and startActivity to exercise activity

--- app/src/main/java/com/example/a7minute_workout/MainActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minute_workout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -22,7 +23,8 @@ class MainActivity : AppCompatActivity() {
         //Can directly call item from xml
         //Don't have to set the object globally can be called using the binding outside of create
         binding?.flStart?.setOnClickListener{
-            Toast.makeText(this@MainActivity, "binding access", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
     }
