af7490493cc67ec4d033f0daa4381aab1627aae1
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-23 17:34:09 -0800

    removed action bar from main screen, by changing it in themes

--- app/src/main/java/com/example/a7minute_workout/MainActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a7minute_workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.fl_start)
+        flStartButton.setOnClickListener{
+
+        }
+
     }
 }
