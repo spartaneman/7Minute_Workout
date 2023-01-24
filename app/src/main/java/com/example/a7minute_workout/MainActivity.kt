3be32281d9bd86c2cab0d4e305ca43cbaa828b0d
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-23 17:51:33 -0800

    overrode the onDestroy to unassign the binding

--- app/src/main/java/com/example/a7minute_workout/MainActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a7minute_workout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import android.widget.Toast
 import com.example.a7minute_workout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +19,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.fl_start)
-        flStartButton.setOnClickListener{
-
+        //Can directly call item from xml
+        //Don't have to set the object globally can be called using the binding outside of create
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this@MainActivity, "binding access", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    //make sure to unassing the binding, to avoid memory leak
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
