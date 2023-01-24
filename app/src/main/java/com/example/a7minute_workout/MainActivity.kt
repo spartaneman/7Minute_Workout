77353b10d12a8a1b8c75b90f36fd31a194ec2461
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-23 17:41:31 -0800

    bound the main activity using view binding and inflating it with layoutInflater and set the content view as the root which is the xml file of activity main

--- app/src/main/java/com/example/a7minute_workout/MainActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.a7minute_workout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.example.a7minute_workout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Creating a viewBinding variable
+    //Activity binding is automatically created for any activity created for the program
+    private var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //This is how we bind the activity
+        //We must inflate it with layoutInflater
+        //root is the xml activity
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         val flStartButton: FrameLayout = findViewById(R.id.fl_start)
         flStartButton.setOnClickListener{
