e0efa603784b6c8b0f3dd5c5475d73654419da77
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-25 17:38:29 -0800

    added a countdown Timer. Currently a rest timer of 30 seconds that runs automatically once we switch to the

--- app/src/main/java/com/example/a7minute_workout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package com.example.a7minute_workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minute_workout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var exBinding: ActivityExerciseBinding? = null
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //view Binding
@@ -25,5 +29,39 @@ class ExerciseActivity : AppCompatActivity() {
         exBinding?.toolBarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setRestProgressBar()
+
+    }
+
+    //This is a timer for rest period
+    private fun setRestProgressBar(){
+        exBinding?.progressTimer?.progress = restProgress
+
+        //restTimer is a countdown timer object that will use the ontick
+        restTimer = object : CountDownTimer(30000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                exBinding?.progressTimer?.progress = 30 - restProgress
+                exBinding?.tvTimer?.text = ("${30-restProgress}")
+            }
+
+            //This automatically runs once the timer has hit zero
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Timer Finished", Toast.LENGTH_LONG).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer != null)
+        {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        //Dont forget that you must destroy the binding at the end
+        exBinding = null
     }
 }
