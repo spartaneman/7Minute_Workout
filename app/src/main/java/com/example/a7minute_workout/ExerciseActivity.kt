ffa84a9d1b8f500349fbb1df43a0d9d92f4a081e
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-25 20:28:57 -0800

    added the second timer using a 2nd framelayout. Remember to use start for the countdown

--- app/src/main/java/com/example/a7minute_workout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minute_workout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.example.a7minute_workout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.example.a7minute_workout.databinding.ActivityExerciseBinding
 
@@ -10,9 +11,10 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var exBinding: ActivityExerciseBinding? = null
     private var restTimer: CountDownTimer? = null
-    private var exerciseTimer: CountDownTimer? = null
+    private var workoutTimer: CountDownTimer? = null
 
     private var restProgress = 0
+    private var workoutProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //view Binding
@@ -37,6 +39,8 @@ class ExerciseActivity : AppCompatActivity() {
 
     //this way it will make sure the progress bar resets after going to another page
     private fun setUpRestView(){
+        exBinding?.flTimer2?.visibility = View.GONE
+        exBinding?.flTimer?.visibility = View.VISIBLE
         if(restTimer != null)
         {
             restTimer?.cancel()
@@ -45,24 +49,58 @@ class ExerciseActivity : AppCompatActivity() {
         setRestProgressBar()
     }
 
+    private fun setupWorkoutView(){
+        exBinding?.flTimer?.visibility = View.GONE
+        exBinding?.tvTitle?.text = "Workout"
+        exBinding?.flTimer2?.visibility = View.VISIBLE
+        if(workoutTimer != null) {
+            workoutTimer?.cancel()
+            workoutProgress = 0
+        }
+        setWorkoutProgressBar()
+    }
+
     //This is a timer for rest period
     private fun setRestProgressBar(){
         exBinding?.progressTimer?.progress = restProgress
 
         //restTimer is a countdown timer object that will use the ontick
-        restTimer = object : CountDownTimer(30000, 1000){
+        restTimer = object : CountDownTimer(10000, 1000){
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
-                exBinding?.progressTimer?.progress = 30 - restProgress
-                exBinding?.tvTimer?.text = ("${30-restProgress}")
+                exBinding?.progressTimer?.progress = 10 - restProgress
+                exBinding?.tvTimer?.text = ("${10-restProgress}")
             }
 
             //This automatically runs once the timer has hit zero
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Timer Finished", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@ExerciseActivity, "Rest Finished", Toast.LENGTH_LONG).show()
+                setupWorkoutView()
+
             }
 
         }.start()
+    }
+
+    private fun setWorkoutProgressBar(){
+        exBinding?.progressTimer2?.progress = workoutProgress
+
+        workoutTimer = object : CountDownTimer(20000, 1000)
+        {
+            override fun onTick(millisUntilFinished: Long) {
+                workoutProgress++
+                exBinding?.progressTimer2?.progress = 20 - workoutProgress
+                exBinding?.tvTimer2?.text = "${20-workoutProgress}"
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Rest Time", Toast.LENGTH_LONG).show()
+                setUpRestView()
+            }
+
+        }.start()
+
+
     }
 
     override fun onDestroy() {
@@ -72,7 +110,11 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer?.cancel()
             restProgress = 0
         }
-
+        if(workoutTimer != null)
+        {
+            workoutTimer?.cancel()
+            workoutProgress = 0
+        }
         //Dont forget that you must destroy the binding at the end
         exBinding = null
     }
